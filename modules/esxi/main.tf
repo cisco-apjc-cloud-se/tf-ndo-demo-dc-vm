1fd6238e9e64ad12e6202cf06e6c071b3ebcc09c
Richard <richwats@cisco.com>
2021-12-15 14:25:30 +1100

    template disk size

--- modules/esxi/main.tf
+++ modules/esxi/main.tf
@@ -102,7 +102,7 @@ resource "vsphere_virtual_machine" "vm" {
 
   disk {
     label            = "disk0"
-    size             = 20
+    size             = data.vsphere_virtual_machine.template.disks.0.size
     eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
     thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
